d62b08f7bcf6c9be63bb11b6e34e35ffa8c4344d
Brian Hartin <brian.hartin@gmail.com>
2014-05-22 01:26:36 -0500

    Brian: Added simple scaling test to prove that a blocking listener is useless at scale.

--- src/test/scala/org/banno/streamstats/BlockingStatusListenerSpec.scala
+++ src/test/scala/org/banno/streamstats/BlockingStatusListenerSpec.scala
@@ -1,6 +1,6 @@
 package org.banno.streamstats
 
-import statistics.{TotalTweets, BlockingStatusListener}
+import statistics.{Statistic, TotalTweets, BlockingStatusListener}
 import twitter4j.Status
 import org.scalatest.{Matchers, FlatSpec}
 import org.scalatest.mock.MockitoSugar
@@ -12,13 +12,41 @@ class BlockingStatusListenerSpec extends BaseSpec {
     val totalTweets = new TotalTweets()
     val stats = List(totalTweets)
     val listener = new BlockingStatusListener(stats)
-    val status = mock[Status]
+    val tweet = mock[Status]
 
     totalTweets.value should be(0) // Sanity check
 
-    listener.onStatus(status)
-    listener.onStatus(status)
+    listener.onStatus(tweet)
+    listener.onStatus(tweet)
 
     totalTweets.value should be(2)
+  }
+
+  it should "probably not scale very well since it blocks until stats are computed" in {
+    val numberOfStats = 10
+    val numberOfTweets = 50
+    val timeToComputeAStatInMs = 10
+
+    val stats = (1 to numberOfStats).map(i => new Statistic {
+      override def compute(status:Status) = {
+        Thread.sleep(timeToComputeAStatInMs)
+      }
+    }).toList
+
+    // Way too long.
+    val expectedTime = (numberOfStats * numberOfTweets * timeToComputeAStatInMs).toDouble
+
+
+    val listener = new BlockingStatusListener(stats)
+    val tweets = (1 to numberOfTweets).map( i => mock[Status])
+    val elapsed = benchmark(tweets.foreach(listener.onStatus(_)))
+    elapsed should be(expectedTime +- (expectedTime * 0.2)) // 20% variance allowed before test fails
+  }
+
+  def benchmark(f: => Any):Double = {
+    val start = System.nanoTime()
+    f
+    val end = System.nanoTime()
+    (end - start) / 1e6
   }
 }
