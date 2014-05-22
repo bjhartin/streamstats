ef72102b6419aca02b93d49a65112b80a7cb7c7f
Brian Hartin <brian.hartin@gmail.com>
2014-05-22 00:55:23 -0500

    Added base spec class to mix in common test traits.

---
+++ src/test/scala/org/banno/streamstats/BaseSpec.scala
@@ -0,0 +1,9 @@
+package org.banno.streamstats
+
+import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.mock.MockitoSugar
+
+class BaseSpec extends FlatSpec with Matchers with MockitoSugar
+{
+
+}
