682a881e7c0858ff8bcf7ae1fc3cd1fe991ed17d
Brian Hartin <brian.hartin@gmail.com>
2014-05-27 23:09:07 -0500

    Added tweets with urls stat.

--- src/main/scala/org/banno/streamstats/statistics/Statistics.scala
+++ src/main/scala/org/banno/streamstats/statistics/Statistics.scala
@@ -5,8 +5,16 @@ import org.banno.streamstats.tweetprocessing._
 
 object Statistics {
   def totalTweets():Stat = (ti:TweetInfo) => {CurrentStats.totalTweets +=1} // Can't use _ for some reason
+
   def tweetsWithEmojis():Stat = (ti:TweetInfo) => {
       CurrentStats.tweetsWithEmojis += (ti(classOf[EmojiExtractor]) match {
+      case Nil => 0
+      case _ => 1
+    })
+  }
+
+  def tweetsWithUrls():Stat = (ti:TweetInfo) => {
+    CurrentStats.tweetsWithUrls += (ti(classOf[UrlExtractor]) match {
       case Nil => 0
       case _ => 1
     })
--- src/test/scala/org/banno/streamstats/statistics/StatisticsSpec.scala
+++ src/test/scala/org/banno/streamstats/statistics/StatisticsSpec.scala
@@ -2,35 +2,45 @@ package org.banno.streamstats.statistics
 
 import mutable.CurrentStats
 import org.banno.streamstats.BaseSpec
-import org.banno.streamstats.tweetprocessing.{EmojiExtractor, Emoji, TweetInfoExtractor}
+import org.banno.streamstats.tweetprocessing._
 
 class StatisticsSpec extends BaseSpec {
   behavior of "Statistics functions"
 
   before {CurrentStats.reset}
 
-  def tweetInfo(emoji:List[Emoji]) = {
+  def tweetInfo(emoji:List[Emoji] = Nil, urls:List[String] = Nil, hashtags: List[String] = Nil) = {
     Map[Class[_ <: TweetInfoExtractor], Any](
-      classOf[EmojiExtractor] -> emoji
+      classOf[EmojiExtractor] -> emoji,
+      classOf[UrlExtractor] -> urls,
+      classOf[HashTagExtractor] -> hashtags
     )
   }
 
   it should "Compute total tweets" in {
     val s = Statistics.totalTweets()
-    s(tweetInfo(Nil))
-    s(tweetInfo(Nil))
+    s(tweetInfo())
+    s(tweetInfo())
     CurrentStats.totalTweets should be(2)
   }
 
-  it should "Compute number of emojis" in {
+  it should "Compute number of tweets with emojis" in {
     val emoji1 = Emoji.allEmoji(0x00AE)
     val emoji2 = Emoji.allEmoji(0x00AE)
 
     val s = Statistics.tweetsWithEmojis()
-    s(tweetInfo(Nil))
+    s(tweetInfo())
     s(tweetInfo(List(emoji1)))
     s(tweetInfo(List(emoji2)))
     CurrentStats.tweetsWithEmojis should be(2)
+  }
+
+  it should "Compute number of tweets with urls" in {
+    val s = Statistics.tweetsWithUrls()
+    s(tweetInfo(Nil))
+    s(tweetInfo(Nil, List("http://learnyouahaskell.com")))
+    s(tweetInfo(Nil, List("http://fpcomplete.com")))
+    CurrentStats.tweetsWithUrls should be(2)
   }
 
 }
