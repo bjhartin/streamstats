abe4f0e8224755abc76038ea6b195eb9df735415
Brian Hartin <brian.hartin@gmail.com>
2014-05-29 18:15:04 -0500

    Splitting a test case that should have been two.

--- src/test/scala/org/banno/streamstats/tweetprocessing/EmojiSpec.scala
+++ src/test/scala/org/banno/streamstats/tweetprocessing/EmojiSpec.scala
@@ -7,7 +7,10 @@ class EmojiSpec extends BaseSpec {
 
   it should "find emoji chars" in {
     Emoji.find(0x00AE.toChar) should be(Some(Emoji(0x00AE.toChar, "REGISTERED SIGN")))
-    Emoji.find(0x0001.toChar) should be(None)
     Emoji.find(0x1F62A.toChar) should be(Some(Emoji(0x1F62A.toChar, "SLEEPY FACE")))
+  }
+
+  it should "return None when char not found" in {
+    Emoji.find(0x0001.toChar) should be(None)
   }
 }
